558033e8b38e4c395055e0402b6df4bae9aabce1
NewMirai <alainanghelidi@gmail.com>
2019-10-22 08:39:08 +0200

    #Final commit & submission

--- horror_movies/analysis.R
+++ horror_movies/analysis.R
@@ -9,12 +9,9 @@ library(patchwork)
 library(lubridate)
 library(ggtext)
 library(waffle)
-library(emojifont)
 library(paletteer)
 library(hrbrthemes)
-library(png)
-library(magick)
-
+library(gganimate)
 
 horror_movies <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-10-22/horror_movies.csv")
 
@@ -30,7 +27,6 @@ font_add_google(name = "Open Sans",family = "os")
 
 showtext_auto()
 
-resume_data <- skimr::skim_to_wide(horror_movies)
 
 
 horror_cleaned <- horror_movies %>% 
@@ -73,18 +69,21 @@ p1 <- top5_countries_year %>%
   scale_fill_paletteer_d(package = ggsci,palette = dark_uchicago,name=NULL)+
   coord_equal()+
   labs(title = glue("USA is dominating the industry of  <span style='color:#BD0707'>**horror movies**</span> <br>"),
-       subtitle = str_wrap(glue("Shown below is a waffle chart showing the top 5 countries
-                                by release of horror movies for each year from 2012 to 2017. 
-                                In 2012, USA released {top5_countries_year_2012_usa} horror movies
-                                against {top5_countries_year_2017_usa} in 2017. It's nearly 2.5 times more horrors movies in 5 years which have been produced!!
-                                Other countries seems to stabilize their number of horrors movies produced."),width = 100),
+       subtitle =glue("Shown below is a waffle chart showing the top 5 countries
+                                by release of horror movies for each year from 2012 to 2017.<br>
+                                In 2012, USA released <span style='color:#BD0707'>**{top5_countries_year_2012_usa}**</span> horror movies
+                                against <span style='color:#BD0707'>**{top5_countries_year_2017_usa}**</span> in 2017. 
+                                It's nearly <span style='color:#BD0707'>**2.5 times**</span> more horrors movies in <span style='color:#BD0707'>**5 years**</span> which have been produced!!<br>
+                                Other countries seems to have stabilized their number of horrors movies produced."),
        caption = "#Tidy Tuesday | Source: IMDB Horror Movie Dataset (Kaggle)| Author: @alangel112407606 inspired by https://github.com/hrbrmstr/waffle")+
   theme_minimal(base_family = "fj")+
   theme(panel.grid = element_blank(), 
         axis.ticks.y= element_line(),
         plot.title = element_markdown(size=24,family ="pm"),
-        plot.subtitle = element_text(size= 14,family = "os"))
+        plot.subtitle = element_markdown(size= 12,family = "os"))
   
 x11(width = 1600 ,height = 900)
 
 p1
+
+
